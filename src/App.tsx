001d998df1a20a4a8f882af047bade02cdb7dc33
Edmundo dos Santos <37958683+Edmundo1100@users.noreply.github.com>
2024-02-16 16:40:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+export const App = () => {
+  return (
+    <>
+      <Header/>
+      <GlobalStyle />
+    </>
+  );
+};
+ 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return <header>
+    {/* <img src="" alt="dt mony"></img> */}
+
+
+    <button type="button">
+        Nova Transação
+    </button>
+    </header>;
+};
